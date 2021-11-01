d2b932fe31629fb8b56700b567cc66f0bdaa7aa6
Mário Rui <marinhocosta@live.com.pt>
2021-11-01 11:04:41 +0000

    vfx updates and collisions fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 
 let spell = new Image();
-spell.src = './images/34.gif'
+spell.src = './images/spell.png'
 
 let harry = new Image();
 harry.src = './images/harry.png'
@@ -14,13 +14,17 @@ let background = new Image();
 background.src = './images/hogwarts.png';
 
 let spellX = 1150, spellY = 350;
-let incX = 5, incY = 2;
+let incX = 5;
 
 let harryX = 0; harryY = 300;
 let isUp = false; isDown = false;
 
 let backgroundMusic = new Audio('./sounds/harrypotter-theme.mp3')
 
+let spells = [
+    {x: spellX, y: spellY},
+    {x: spellX, y: spellY + 200}
+]
 
 let intervalId = 0;
 let isGameOver = false;
@@ -33,31 +37,49 @@ const draw = () => {
     
     ctx.drawImage(harry, harryX, harryY)
 
-    ctx.drawImage(spell, spellX, spellY)
-
     ctx.drawImage(voldemort, 1300, 280, 200, 200) 
 
-    backgroundMusic.play()
+    for (let i = 0; i<spells.length; i++){
+        ctx.drawImage(spell, spells[i].x, spells[i].y)  
+        
+        if (spells[i].x + spell.width < 0){
+            spells[i].x = 1150
+            spells[i].y = Math.floor(Math.random() * canvas.height)
+        }
+    }
+
+    //backgroundMusic.play()
     backgroundMusic.volume = 1
 }
 
 const collision = () => {
-    if((harryX + harry.width >= spellX + spell.height + 50) && (harryX + harry.width >= spellY + spellY.height)){ //collisions still not right 
-        isGameOver = true
+    for (let i = 0; i<spells.length; i++){
+        if((harryX + harry.width >= spells[i].x + 60) && (harryY < spells[i].y && harryY + harry.height > spells[i].y + spell.height)){  //checking (X) checking (Y)
+            isGameOver = true
+        }
     }
+
 }
+
+
 
 const animation = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    draw()
-    spellX = spellX - incX
+    draw()       
+
+
+    for (let i = 0; i<spells.length; i++){
+        //let increasing = setInterval((incX = incX++), 1000)
+        spells[i].x = spells[i].x - incX
+    }
+  
 
     if (isUp){
         harryY = harryY - 10
     } else if (isDown){
         harryY = harryY + 10
     }
-    
+
     collision()
 
     if (isGameOver){
@@ -66,6 +88,7 @@ const animation = () => {
     } else {
         intervalId = requestAnimationFrame(animation)
     } 
+    
 }
 
 animation()
